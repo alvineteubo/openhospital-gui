3a74224043855bf9fe4406df9c328be3a08f4bb5
Alvine TEUBO <alvineteubo@gmail.com>
2024-11-23 09:57:43 +0100

    ajout du copyright

--- src/main/java/org/isf/reductionplan/gui/ReductionPlanBrowser.java
+++ src/main/java/org/isf/reductionplan/gui/ReductionPlanBrowser.java
@@ -1,4 +1,28 @@
+
+/*
+ * Open Hospital (www.open-hospital.org)
+ * Copyright © 2006-2024 Informatici Senza Frontiere (info@informaticisenzafrontiere.org)
+ *
+ * Open Hospital is a free and open source software for healthcare data management.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * https://www.gnu.org/licenses/gpl-3.0-standalone.html
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 package org.isf.reductionplan.gui;
+
+
 
 import java.awt.BorderLayout;
 import java.util.List;
@@ -14,7 +38,6 @@ import org.isf.generaldata.MessageBundle;
 import org.isf.menu.manager.Context;
 import org.isf.reductionplan.manager.ReductionplanBrowserManager;
 import org.isf.reductionplan.model.ReductionPlan;
-import org.isf.supplier.gui.SupplierBrowser;
 import org.isf.utils.exception.OHServiceException;
 import org.isf.utils.exception.gui.OHServiceExceptionUtil;
 import org.isf.utils.jobjects.ModalJFrame;
